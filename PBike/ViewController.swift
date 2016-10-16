620307a0e06e3fe9db98765dd6c4d64eb88fc47d
陳 冠禎 <vul354@terrys-macbook-pro.local>
2016-10-16 16:35:04 +0800

    Initial Commit

---
+++ PBike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PBike
+//
+//  Created by 陳 冠禎 on 2016/10/16.
+//  Copyright © 2016年 陳 冠禎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
