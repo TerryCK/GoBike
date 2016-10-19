7c317bdd07d91fafff7f82cab2b89e817af78ac9
陳 冠禎 <vul354@Terrys-MacBook-Pro.local>
2016-10-19 16:47:26 +0800

    currentLocation is fine & mapView is fine

---
+++ PBike/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  PBike
+//
+//  Created by 陳 冠禎 on 2016/10/19.
+//  Copyright © 2016年 陳 冠禎. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
